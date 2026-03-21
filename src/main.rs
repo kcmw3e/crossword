ee3a591578a3fc1a89c19418f10c4d1504781071
Casey Walker <kcmw3e@gmail.com>
2026-03-21 14:17:55 -0400

    Add hello-world starting point

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//! Command line program to generate a crossword puzzle from a list of words.
+//!
+//! ----------------------------------------------------------------------------
+
+fn main() {
+    println!("Hello, world!");
+}
